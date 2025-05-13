c023572673c3a6d5909d6a2d8ed9c5ea331e6019
aleroboyo <aleroboyo0@gmail.com>
2025-05-13 20:30:47 +0100

    Update Portfolio Repo

---
+++ main.js
@@ -0,0 +1,28 @@
+/*function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('active');
+
+
+}*/
+
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    const hamburger = document.querySelector('.hamburger');
+    menu.classList.toggle('active');
+    hamburger.classList.toggle('active');
+}
+
+function setMoodBackground() {
+    const hour = new Date().getHours();
+    const body = document.body;
+
+    if (hour >= 6 && hour < 12) {
+        body.classList.add('morning');
+    } else if (hour >= 12 && hour < 18) {
+        body.classList.add('afternoon');
+    } else {
+        body.classList.add('night');
+    }
+}
+
+window.onload = setMoodBackground;
